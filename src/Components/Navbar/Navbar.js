10b1be440b9e7be2ff6664aa07a2882a07e445d2
Gowtham <gowthamvenkatesh.gv@gmail.com>
2021-06-17 21:49:04 +0530

    main section and nav bar changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+
+const NavbarItems = (props) => {
+  return(
+    <React.Fragment>
+      <a href="">
+        <FontAwesomeIcon icon={props.icon}/> 
+        <p>{props.text}</p>
+      </a>
+    </React.Fragment>
+  )
+}
+
 export const Navbar = (props) => {
+  const navBarInfo = [
+    {
+      text: 'about me',
+      icon: faUser
+    },
+    {
+      text: 'skills',
+      icon: faLaptopCode
+    },
+    {
+      text: 'experience',
+      icon: faBriefcase
+    },
+    {
+      text: 'contact me',
+      icon: faPhone
+    }
+  ];
+  const listItems = navBarInfo.map((navBarItem) =>
+    <li className="navbar-list">
+      <NavbarItems 
+        key={navBarItem.text} 
+        text={navBarItem.text} 
+        icon ={navBarItem.icon}
+      />
+    </li>
+  );
   return(
   <nav className="nav-bar">
     <ul>
-      <li><a href="">about</a></li>
-      <li><a href="">skills</a></li>
-      <li><a href="">experience</a></li>
-      <li><a href="">contact me</a></li>
+      {listItems}
     </ul>
   </nav>
   );
